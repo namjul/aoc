c12ba0f5352f8eeacc2455e3b76b3472a7fe838c
Samuel Hobl <nam@hobl.at>
2020-12-10 10:22:48 +0100

    day10: add part 2

--- 2020/day10.lua
+++ 2020/day10.lua
@@ -12,15 +12,42 @@ file:close()
 table.sort(lines, function(a,b)
   return a < b and true or false
 end)
-table.insert(lines, math.max(table.unpack(lines)) + 3)
+table.insert(lines, 1, 0)
+table.insert(lines, lines[#lines] + 3)
 
 print(inspect(lines))
 print('--')
 
-local differences = {}
-for index,value in ipairs(lines) do
-    local difference = tostring(value - (lines[index - 1] or 0))
-    differences[difference] = (differences[difference] or 0) + 1
-end
+-- part1
+local function calcDifferences()
+  local differences = {}
+  for index,value in ipairs(lines) do
+      local difference = tostring(value - (lines[index - 1] or 0))
+      differences[difference] = (differences[difference] or 0) + 1
+  end
 
-print(inspect(differences), differences['1'] * differences['3'])
+  print(inspect(differences), differences['1'] * differences['3'])
+end
+calcDifferences()
+
+-- part2
+local cache = {}
+local function countCombinations(from)
+  if cache[from] then
+    return cache[from]
+  end
+  local count = 0
+  local next = from + 1
+
+  while lines[next] and lines[next] - lines[from] <= 3 do
+    count = count + countCombinations(next)
+    next = next + 1
+  end
+
+  count = count == 0 and 1 or count
+
+  cache[from] = count
+
+  return count
+end
+print(inspect(countCombinations(1)))
