3532d8628cf8b1fa041af30e65f9d056761b01e5
Samuel Hobl <nam@hobl.at>
2020-12-11 18:54:04 +0100

    day11: update

--- 2020/day11.lua
+++ 2020/day11.lua
@@ -17,22 +17,22 @@ file:close()
 
 local seats = {}
 for y, row in ipairs(rows) do
-  seats[y] = seats[y] or {}
-
   local x = 1
   for character in string.gmatch(row, '.') do
     seats[x..':'..y] = character
-    seats[y][x] = character
     x = x + 1
   end
 end
+
+-- print(inspect(seats))
+-- print('--')
 
 local function printSeats(seats)
   print('--')
   for y,row in ipairs(rows) do
     local x = 1
     for _ in string.gmatch(row, '.') do
-      io.write(seats[y][x])
+      io.write(seats[x..':'..y])
       x = x + 1
     end
     io.write('\n')
@@ -40,46 +40,19 @@ local function printSeats(seats)
   print('--')
 end
 
--- print(printSeats(seats))
--- print('--')
-
-
 local function coordinateKey(coordinate)
   return math.floor(coordinate[1])..':'..math.floor(coordinate[2])
 end
 
-local function run(seats, lookupFn)
-  local newSeats = {}
-  local changes = false
-
-  for y,row in ipairs(seats) do
-   newSeats[y] = newSeats[y] or {}
-    for x,seat in ipairs(row) do
-      newSeats[x] = newSeats[x] or {}
-      local neighborCount = lookupFn(seats, {x, y})
-      if seat == 'L' and neighborCount == 0 then
-        changes = true
-        newSeats[y][x] = '#'
-      elseif seat == '#' and neighborCount >= 5 then
-        changes = true
-        newSeats[y][x] = 'L'
-      else
-        newSeats[y][x] = seat
-      end
-    end
-  end
-  return newSeats, changes
-end
-
 local function lookupSurround(seats, origin)
-  local x, y = table.unpack(origin)
+  local x, y = utils.split(origin, ':')
   local adjacentSeats = {
     {x-1,y-1}, {x,y-1}, {x+1,y-1},
     {x-1,y},            {x+1,y},
     {x-1,y+1}, {x,y+1}, {x+1,y+1}
   }
   return #utils.filter(adjacentSeats, function (adjacentSeat)
-    return (seats[adjacentSeat[2]] or {})[adjacentSeat[1]] == '#'
+    return seats[coordinateKey(adjacentSeat)] == '#'
   end)
 
 end
@@ -95,12 +68,11 @@ local function lookupDiagonal(seats, origin)
   for _, direction in ipairs(directions) do
     local dx = direction[1]
     local dy = direction[2]
-    local x, y = table.unpack(origin)
+    local x, y = utils.split(origin, ':')
     while true do
-      x = math.floor(x + dx)
-      y = math.floor(y + dy)
-      local seat = (seats[y] or {})[x]
-      -- print('origin: '..inspect(origin), 'x: '..x, 'y: '..y, 'seat: '..(seat or 'nil'))
+      x = x + dx
+      y = y + dy
+      local seat = seats[coordinateKey({x,y})]
       if seat == nil then
         table.insert(directionSeats, '.')
         break
@@ -114,17 +86,33 @@ local function lookupDiagonal(seats, origin)
         break
       end
     end
-    -- print('directionSeat: ', inspect(directionSeats))
   end
-    -- print('-')
   return #utils.filter(directionSeats, function (v) return v == '#' end)
+end
+
+local function run(seats, lookupFn, neighborCountMax)
+  local newSeats = {}
+  local changes = false
+
+  for coordinate, seat in pairs(seats) do
+    local neighborCount = lookupFn(seats, coordinate)
+    if seat == 'L' and neighborCount == 0 then
+      changes = true
+      newSeats[coordinate] = '#'
+    elseif seat == '#' and neighborCount >= neighborCountMax then
+      changes = true
+      newSeats[coordinate] = 'L'
+    else
+      newSeats[coordinate] = seat
+    end
+  end
+  return newSeats, changes
 end
 
 local finished = false
 local currentSeats = utils.clone(seats)
 while not finished do
-  local newSeats, changes = run(currentSeats, lookupDiagonal)
-  printSeats(newSeats)
+  local newSeats, changes = run(currentSeats, lookupDiagonal, 5)
   if changes then
     currentSeats = newSeats
   else
@@ -132,8 +120,4 @@ while not finished do
   end
 end
 
-local sum = utils.sum(utils.map(currentSeats, function (row)
-  return #utils.filter(row, function (v) return v == '#' end)
-end))
-
-print(sum)
+print(#utils.filter(currentSeats, function (v) return v == '#' end))
