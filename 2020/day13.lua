90fea0a90f0fac758075ac7d93381a36edb1ab46
Samuel Hobl <nam@hobl.at>
2020-12-13 08:45:21 +0100

    day13: part 2
    but too slow to calculate big numbers

--- 2020/day13.lua
+++ 2020/day13.lua
@@ -5,7 +5,8 @@ local inspect = require('inspect')
 
 local iterator = utils.day(13)
 local timestamp = tonumber(iterator())
-local busIds = utils.filter({utils.split(iterator(), ',')}, function (value) return value ~= 'x' end)
+local busIds = {utils.split(iterator(), ',')}
+local busIdsWithoutX = utils.filter(busIds, function (value) return value ~= 'x' end)
 
 print(timestamp)
 print(inspect(busIds))
@@ -13,7 +14,7 @@ print('--')
 
 local function departureTime()
   local times = {}
-  for _, busId in ipairs(busIds) do
+  for _, busId in ipairs(busIdsWithoutX) do
     table.insert(times, math.ceil(timestamp / tonumber(busId)) * tonumber(busId))
   end
 
@@ -24,7 +25,34 @@ local function departureTime()
   return math.min(table.unpack(times)), index
 end
 
-local minDepartedTime, index = departureTime()
+-- part1
+local minDepartedTime, lookup = departureTime()
 local waitingTime = minDepartedTime - timestamp
+print(waitingTime * busIdsWithoutX[lookup[minDepartedTime]])
 
-print(waitingTime * busIds[index[minDepartedTime]])
+-- part2
+
+local function earliestTime(list)
+  local time = 0
+  while true do
+    local count = 0
+    for index, busId in ipairs(list) do
+      if busId ~= 'x' then
+        local minute = index - 1
+        if (time + minute) % tonumber(busId) == 0 then
+          count = count + 1
+        else
+          break
+        end
+      end
+    end
+
+    if count >= #utils.filter(list, function (value) return value ~= 'x' end) then
+      return time
+    end
+
+    time = time + list[1]
+  end
+end
+
+print(earliestTime(busIds))
