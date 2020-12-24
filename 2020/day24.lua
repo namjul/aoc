5cf387526a92d5a66f7d625b20a48b0ae6fc520b
Samuel Hobl <nam@hobl.at>
2020-12-24 13:09:04 +0100

    day24: part2

--- 2020/day24.lua
+++ 2020/day24.lua
@@ -1,7 +1,7 @@
 local utils = require('utils')
 local inspect = require('inspect')
 
-local blackTiles = {}
+local tiles = {}
 
 local paths = {}
 
@@ -35,8 +35,11 @@ local directions = {
   ne = {1,-1}
 }
 
+local function hash(v)
+  return v[1]..':'..v[2]
+end
+
 for _, path in ipairs(paths) do
-  -- print(inspect(path))
 
   local reference = {0, 0}
   for i=1,#path do
@@ -45,12 +48,83 @@ for _, path in ipairs(paths) do
     reference = {reference[1]+q, reference[2]+r}
   end
 
-  local hashKey = reference[1]..':'..reference[2]
-  if not blackTiles[hashKey] then
-    blackTiles[hashKey] = true
+  local hashKey = hash(reference)
+  if tiles[hashKey] == nil then
+    tiles[hashKey] = true
   else
-    blackTiles[hashKey] = false
+    tiles[hashKey] = nil
   end
 end
 
-print(inspect(#utils.filter(blackTiles, function(value) return value end)))
+local function unhash(v)
+  local q, r = utils.split(v, ':')
+  return { tonumber(q), tonumber(r) }
+end
+
+-- part1
+-- print(inspect(utils.len(tiles)))
+
+local checked = {}
+local function checkCoordinate(coordinateKey, result)
+  if not checked[coordinateKey] then
+    -- print(coordinateKey)
+    checked[coordinateKey] = true
+  else
+    -- print('already checked', coordinateKey)
+  end
+  local coordinate = unhash(coordinateKey)
+  -- check neighbors for coordinate
+  local neighbors = utils.map(directions, function(direction)
+    return hash({coordinate[1] + direction[1], coordinate[2] + direction[2]})
+  end)
+
+  local blackNeighbors = utils.filter(neighbors, function(neighbor)
+    return tiles[neighbor]
+  end)
+
+  local whiteNeighbors = utils.filter(neighbors, function(neighbor)
+    return tiles[neighbor] == nil
+  end)
+
+  if coordinateKey == '-2:0' then
+    -- print('blackNeighbors', coordinateKey, tiles[coordinateKey] and 'black' or 'white', inspect(blackNeighbors), #blackNeighbors)
+  end
+
+  if tiles[coordinateKey] then
+    -- black
+    if #blackNeighbors == 0 or #blackNeighbors > 2 then
+      -- print('black to white')
+      result[coordinateKey] = nil
+    else
+      result[coordinateKey] = true
+    end
+  else
+    -- white
+    if #blackNeighbors == 2 then
+      -- print('white to black')
+      result[coordinateKey] = true
+    else
+      result[coordinateKey] = nil
+    end
+  end
+end
+
+for _=1,100 do -- run days
+  local newTiles = {}
+  for coordinateKey in pairs(tiles) do
+    checkCoordinate(coordinateKey, newTiles)
+
+    -- check neighbors
+    local coordinate = unhash(coordinateKey)
+    local neighbors = utils.map(directions, function(direction)
+      return hash({coordinate[1] + direction[1], coordinate[2] + direction[2]})
+    end)
+    for _, neighborKey in pairs(neighbors) do
+        checkCoordinate(neighborKey, newTiles)
+    end
+  end
+  tiles = newTiles
+  print('Day '.._..':', inspect(#utils.filter(tiles, function(value) return value end)))
+end
+
+print('endresult: ', inspect(#utils.filter(tiles, function(value) return value end)))
