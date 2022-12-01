29130ce0ce49f20832882c76ab1a725f3abd7546
Samuel Hobl <samuel.hobl@gmail.com>
2022-12-01 10:24:14 +0100

    enhance(rust): day1

--- 2022/src/day1.rs
+++ 2022/src/day1.rs
@@ -1,13 +1,11 @@
-pub fn run(input: Vec<&str>) -> (u32, u32) {
+pub fn run(input: Vec<u32>) -> (u32, u32) {
     let mut vec: Vec<u32> = Vec::new();
-    let mut current_group: u32 = 0;
-    for line in input {
-        if line != "" {
-            let x: u32 = line.parse().unwrap();
-            current_group = current_group + x;
-        } else {
-            vec.push(current_group);
-            current_group = 0;
+    let mut value: u32 = 0;
+    for num in input {
+        value = value + num;
+        if num == 0 {
+            vec.push(value);
+            value = 0;
         }
     }
     vec.sort();
--- 2022/src/main.rs
+++ 2022/src/main.rs
@@ -3,9 +3,12 @@ use std::io;
 mod day1;
 
 fn main() -> io::Result<()> {
-    let contents =
+    let content =
         fs::read_to_string("./src/day1.txt").expect("Should have been able to read the file");
-    let input: Vec<&str> = contents.split("\n").collect();
+    let input: Vec<u32> = content
+        .split("\n")
+        .map(|x| x.parse::<u32>().unwrap_or(0))
+        .collect();
     let output = day1::run(input);
     println!("{:#?}", output);
     Ok(())
