c56843e930f9469d6f34e34402b04d59208e1c62
Samuel Hobl <samuel.hobl@gmail.com>
2022-12-04 13:26:58 +0100

    enhance(rust): day4

--- 2022/src/day4.rs
+++ 2022/src/day4.rs
@@ -1,13 +1,12 @@
 use std::ops::Range;
 
-fn containsFully(r1: Range<u32>, r2: Range<u32>) -> bool {
+fn contains_fully(r1: Range<u32>, r2: Range<u32>) -> bool {
     let a = r1.start;
     let b = r1.end;
     let c = r2.start;
     let d = r2.end;
 
-    // had to look this up https://www.reddit.com/r/adventofcode/comments/zc557b/2022_day_4_of_max_and_min/
-    if c <= a && a <= b && b <= d || (a <= c) && (c <= d) && d <= b {
+    if a <= c && d <= b || a >= c && b <= d {
         return true;
     }
 
@@ -37,7 +36,8 @@ pub fn run(input: Vec<&str>) -> Option<(u32, u32)> {
             let first_range = to_range(first);
             let second_range = to_range(second);
 
-            if containsFully(first_range, second_range) {
+            if contains_fully(first_range, second_range) {
+                println!("{}", line);
                 return Some(true);
             }
 
