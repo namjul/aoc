b3fe93b7a4e1198662453969adc2fcd50ba374ad
Samuel Hobl <samuel.hobl@gmail.com>
2022-12-04 19:25:38 +0100

    refactor: use binary targets

---
+++ 2022/src/bin/day4.rs
@@ -0,0 +1,56 @@
+use aoc_2022::read;
+use std::io;
+use std::ops::Range;
+
+fn contains_fully(r1: Range<u32>, r2: Range<u32>) -> bool {
+    let a = r1.start;
+    let b = r1.end;
+    let c = r2.start;
+    let d = r2.end;
+
+    if a <= c && d <= b || a >= c && b <= d {
+        return true;
+    }
+
+    return false;
+}
+
+pub fn main() -> io::Result<()> {
+    return read("./src/bin/day4.txt")
+        .map(|line| {
+            let result1 = line
+                .iter()
+                .filter_map(|line| {
+                    let to_range = |range: &str| {
+                        return range
+                            .split_once("-")
+                            .map(|(first, last)| {
+                                return first
+                                    .parse()
+                                    .and_then(|first_num: u32| {
+                                        last.parse().map(|last_num: u32| (first_num..last_num + 1))
+                                    })
+                                    .unwrap();
+                            })
+                            .unwrap();
+                    };
+
+                    let (first, second) = line.split_once(",").unwrap();
+
+                    let first_range = to_range(first);
+                    let second_range = to_range(second);
+
+                    if contains_fully(first_range, second_range) {
+                        return Some(true);
+                    }
+
+                    return None;
+                })
+                .count();
+            return result1;
+        })
+        .map(|result| {
+            println!("Result: {}", result);
+            return ();
+        });
+}
