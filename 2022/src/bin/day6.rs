ab008a542f80c668d4c3bd2354796fe6ac428ade
Samuel Hobl <samuel.hobl@gmail.com>
2022-12-12 10:30:34 +0100

    feat(rust, 2022): day6 part2

--- 2022/src/bin/day6.rs
+++ 2022/src/bin/day6.rs
@@ -8,15 +8,17 @@ pub fn main() -> io::Result<()> {
 
         let chars = datastream.chars().into_iter().collect::<Vec<_>>();
 
+        let start_of_message = 14;
+
         for (index, _char) in chars.iter().enumerate() {
-            let cursor = if index >= 4 { index - 3 } else { 0 };
+            let cursor = if index >= start_of_message { index - (start_of_message - 1) } else { 0 };
 
             let mut set = HashSet::new();
             for n in cursor..(index + 1) {
                 set.insert(chars[n]);
             }
 
-            if set.len() == 4 {
+            if set.len() == start_of_message {
                 println!("found marker at {}", index + 1);
                 return ();
             }
